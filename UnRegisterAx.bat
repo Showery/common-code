173e09c9bb9729030ff648c6e7862fcdd9af265d
Showery <yfhall@163.com>
2013-07-24 15:05:04 +0800

    修改UnRegister.bat

--- UnRegisterAx.bat
+++ UnRegisterAx.bat
@@ -1,9 +1,11 @@
 cd /d %~dp0
 :: -----------UnRegister XD source filter.------------
 regsvr32 -u -s XDSource.ax
+del XDSource.ax /f /q
 
 :: ----------UnRegister Core AVC Decoder.----------
 regsvr32 -u -s CoreAVCDecoder.ax
+del CoreAVCDecoder.ax /f /q
 
 :: -----UnRegistry information about  CoreAVCDecoder.------
 REG DELETE HKLM\SOFTWARE\CoreCodec /f
@@ -14,3 +16,10 @@ regsvr32 -u -s mkx.dll
 regsvr32 -u -s mp4.dll
 regsvr32 -u -s ts.dll
 regsvr32 -u -s splitter.ax
+del avi.dll /f /q
+del mkx.dll /f /q
+del mp4.dll /f /q
+del ts.dll /f /q
+del splitter.ax /f /q
+del mkunicode.dll /f /q
+del mkzlib.dll /f /q
