47a9ad26766ad2f1edca675319eb9f1371db894e
Showery <yfhall@163.com>
2013-08-26 18:58:03 +0800

    [RegisterAx.bat] + 添加判断文件是否存在的代码。

--- RegisterAx.bat
+++ RegisterAx.bat
@@ -1,5 +1,8 @@
 cd /d %~dp0
 :: -----------Register XD source filter------------
+if not exist XDSource.ax (
+   msg %username% XDSource.ax 文件不存在
+)
 regsvr32  -s XDSource.ax
 
 :: ------------------Add e-mail and Serial for CoreCodec.-----------------------
@@ -8,11 +11,35 @@ REG ADD "HKLM\SOFTWARE\CoreCodec\CoreAVC Pro 3.x" /f
 REG ADD "HKLM\SOFTWARE\CoreCodec\CoreAVC Pro 3.x" /v User /t REG_SZ /d rbogaar@aol.com /f
 REG ADD "HKLM\SOFTWARE\CoreCodec\CoreAVC Pro 3.x" /v Serial /t REG_SZ /d TBCYS8-Q18CKZ-8FHGG8-F49LTA-UK8AQQ /f
 :: --------------Register Core AVC decoder.----------------
+if not exist CoreAVCDecoder.ax (
+   msg %username% CoreAVCDecoder.ax 文件不存在
+)
 regsvr32 -s CoreAVCDecoder.ax
 
+:: --------------Register XD MJPEG Decoder.----------------
+if not exist XDJpegDecoder.ax (
+   msg %username% XDJpegDecoder.ax 文件不存在
+)
+regsvr32 -s XDJpegDecoder.ax
+
 :: --------------Register haali dll or ax----------------
+if not exist avi.dll (
+   msg %username% avi.dll 文件不存在
+)
 regsvr32 -s avi.dll
+if not exist avi.dll (
+   msg %username% avi.dll 文件不存在
+)
 regsvr32 -s mkx.dll
+if not exist mkx.dll (
+   msg %username% mkx.dll 文件不存在
+)
 regsvr32 -s mp4.dll
+if not exist mp4.dll (
+   msg %username% mp4.dll 文件不存在
+)
 regsvr32 -s ts.dll
+if not exist ts.dll (
+   msg %username% ts.dll 文件不存在
+)
 regsvr32 -s splitter.ax
--- UnRegisterAx.bat
+++ UnRegisterAx.bat
@@ -1,4 +1,5 @@
 cd /d %~dp0
+
 :: -----------UnRegister XD source filter.------------
 regsvr32 -u -s XDSource.ax
 del XDSource.ax /f /q
@@ -6,9 +7,12 @@ del XDSource.ax /f /q
 :: ----------UnRegister Core AVC Decoder.----------
 regsvr32 -u -s CoreAVCDecoder.ax
 del CoreAVCDecoder.ax /f /q
-
 :: -----UnRegistry information about  CoreAVCDecoder.------
 REG DELETE HKLM\SOFTWARE\CoreCodec /f
+
+:: ----------UnRegister XD MJPEG Decoder.----------
+regsvr32 -u -s XDJpegDecoder.ax
+del XDJpegDecoder.ax /f /q
 
 :: --------------UnRegister haali dll or ax----------------
 regsvr32 -u -s avi.dll
