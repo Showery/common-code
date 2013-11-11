7388df69b3e2029c833dc0d6f4c5133de5f5f530
Showery <yfhall@163.com>
2013-11-11 16:07:40 +0800

    [常用代码][showery-safe-free.h] * 用nullptr替换NULL

--- showery-safe-free.h
+++ showery-safe-free.h
@@ -3,25 +3,28 @@
 template< typename T_Delete >
 inline void SafeDelete( T_Delete **pp )
 {
-    if ( *pp!=NULL )
-        delete ( *pp );
-    *pp = NULL;
+    if(nullptr!=pp && nullptr!=*pp) {
+        delete (*pp);
+        *pp = nullptr;
+    }
 }
 
 template< typename T_Delete >
 inline void SafeDeleteArray( T_Delete **pp)
 {
-    if ( *pp!=NULL )
-        delete []( *pp );
-    *pp = NULL;
+    if(nullptr!=pp && nullptr!=*pp) {
+        delete [](*pp);
+        *pp = nullptr;
+    }
 }
 
 template< typename T_Release >
 inline void SafeRelease( T_Release **pp )
 {
-    if ( *pp!=NULL )
-    {
-        ( *pp )->Release();
-        *pp = NULL;
+    ULONG ret = 0;
+    if(nullptr!=pp && nullptr!=*pp) {
+        ret = (*pp)->Release();
+        *pp = nullptr;
     }
+    return ret;
 }
